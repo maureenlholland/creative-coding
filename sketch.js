b9c9486d74bbd9abd025d9a9e3349cdd54258b6e
maureenlholland <maureen.holland11@gmail.com>
2021-03-01 11:05:33 +0000

    canvas api basics

--- sketch.js
+++ sketch.js
@@ -1,13 +1,27 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [2048, 2048],
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+
+    context.beginPath();
+    // angles are in radians, not degrees
+    context.arc(width / 2, height / 2, 250, 0, Math.PI * 2, false);
+    context.fillStyle = "blue";
+    context.fill();
+
+    context.beginPath();
+    context.arc(width / 2, height / 2, 250, 0, Math.PI, false);
+    context.fillStyle = "lightblue";
+    context.fill();
+    context.strokeStyle = "red";
+    context.lineWidth = 10;
+    context.stroke();
   };
 };
 
